21319c409fa980dbefd869976e462641825373a7
charity <wa.nelimacharity@gmail.com>
2022-12-16 17:21:39 +0300

    added column username and id number to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_16_135808) do
+ActiveRecord::Schema.define(version: 2022_12_16_142049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2022_12_16_135808) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "id_number"
+    t.string "username"
   end
 
   create_table "vehicles", force: :cascade do |t|
