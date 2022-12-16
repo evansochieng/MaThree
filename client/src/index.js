3d89b5ed88377fb21fbf37f81a0a70f553fd1dd2
evans ochieng <evansochieng121@gmail.com>
2022-12-16 18:44:32 +0300

    create the nav bar and routes for other pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <Router>
+  //     <App />
+  //   </Router>
+  // </React.StrictMode>,
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
