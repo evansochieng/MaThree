99d4e7b7040a657c7fd36a367f5ba0610f0e4003
evans ochieng <evansochieng121@gmail.com>
2022-12-23 02:49:20 +0300

    run migration for orders model, add code for routes

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -2,10 +2,21 @@ import React from 'react'
 
 function Book() {
   return (
-    <div>
-        Booking page
-    </div>
-  )
+    <div>Booking page</div>
+  );
 }
 
 export default Book
+
+{/* <>
+      <div style={{ color: "white", paddingTop: "200px" }}>
+        <caption>Book our comfortable buses for your trip to and from work</caption>
+        <select name="routes" id="routes">
+          <option value="Thika Road 1">Thika-UpperHill</option>
+          <option value="Waiyaki Way 1">Kikuyu-UpperHill</option>
+          <option value="Jogoo Road 1">Donholm-UpperHill</option>
+          <option value="Mombasa Road 1">Kitengela-UpperHill</option>
+          <option value="Ngong Road 1">Karen-Westlands</option>
+        </select>
+      </div>
+    </> */}
