73f8c55d62d22002486737f082912f24205c821c
Shanks <onyangogeorge954@gmail.com>
2022-12-23 11:36:57 +0300

    add booking page

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Info from './Form/Info'
+import Payment from './Form/Payment'
+import Summary from './Form/Summary'
+import Stepper from './Stepper'
+import StepperControl from './StepperControl'
 
 function Book() {
+
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const steps = [
+    "Personal Information",
+    "Payment",
+    "Complete"
+  ]
+
+  const displaystep = (step) => {
+    switch (step) {
+      case 1:
+        return <Info />;
+      case 2:
+        return <Payment />;
+      case 3:
+        return <Summary />;
+      default:
+    }
+  }
+
   return (
-    <div>
-        Booking page
+    <div className='book-container'>
+      <div className='book-wrapper'>
+        <div className='book-route'>
+          <Stepper
+            steps={steps}
+            currentStep={currentStep}
+          />
+        </div>
+        <StepperControl />
+      </div> 
     </div>
   )
 }
---
+++ client/src/components/Stepper.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState, useRef } from "react";
+
+const Stepper = ({ steps, currentStep }) => {
+  const [newStep, setNewStep] = useState([]);
+  const stepRef = useRef();
+
+  const updateStep = (stepNumber, steps) => {
+    const newSteps = [...steps];
+    let count = 0;
+
+    while (count < newSteps.length) {
+      // current step
+      if (count === stepNumber) {
+        newSteps[count] = {
+          ...newSteps[count],
+          highlighted: true,
+          selected: true,
+          completed: true,
+        };
+        count++;
+      }
+      //step completed
+      else if (count < stepNumber) {
+        newSteps[count] = {
+          ...newSteps[count],
+          highlighted: false,
+          selected: true,
+          completed: true,
+        };
+        count++;
+      }
+      // step pending
+      else {
+        newSteps[count] = {
+          ...newSteps[count],
+          highlighted: false,
+          selected: false,
+          completed: false,
+        };
+        count++;
+      }
+    }
+    return newSteps;
+  };
+
+  useEffect(() => {
+    //create object
+    const stepsState = steps.map((step, index) =>
+      Object.assign(
+        {},
+        {
+          description: step,
+          completed: false,
+          highlighted: index === 0 ? true : false,
+          selected: index === 0 ? true : false,
+        }
+      )
+    );
+
+    stepRef.current = stepsState;
+    const current = updateStep(currentStep - 1, stepRef.current);
+    setNewStep(current);
+  }, [steps, currentStep]);
+
+  const displaySteps = newStep.map((step, index) => {
+    return (
+      <div
+        key={index}
+        className={
+          index !== newStep.length - 1 ? "step-wrapper" : "alt-wrapper"
+        }
+      >
+        <div className="step-items">
+          <div className="dis-num">1</div>
+          <div className="dis-desc">Description</div>
+        </div>
+        <div className="dis-line">{/*Display line*/}</div>
+      </div>
+    );
+  });
+
+  return <div className="step-layout">{displaySteps}</div>;
+};
+
+export default Stepper;
