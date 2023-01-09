e16655f5af5c2f7096ba9e748c0e9da56e0e7883
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2023-01-09 23:18:34 +0300

    contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -2,10 +2,48 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>
-        <p>Our contacts will go here</p>
-    </div>
-  )
-}
+    <div className="about-container">
+    <div className="about-layout">
+      <h1 className="about_us">CONTACTS</h1>
+      <div>
+        <section className="mission_vision">
+         
+          <div className="mission_container">
+            <p className="mission">OUR OFFICES</p>
+            <p>
+              Our offices are based in Nairobi in Garden City Business Park,
+              3rd Floor, office number 309
 
-export default Contact
+            </p>
+            <p>Our team  led by our C.E.O  Dr. Ochieng Evans and his  deputy Dr. Mr Onyango George</p>
+            <p>The rest of the administration team is a follows:
+              <li>
+               <ul> Madam Charity Nelima: Chief Operational Manager and handles all administrative roles</ul>
+              <ul>  Prof. Riddick Angela : Head of Company's displinary Commitee</ul>
+              <ul> Mr Nthiga Denis: Directing Manager </ul>
+              </li>
+            </p>
+
+            <p> incase of any complains  or inquires on  the company's operations 
+              you  can reach us  via 
+              <li>
+                <ul>
+                  office Number: +254 704-999-704
+                </ul>
+                <ul> Email : mathree@gmail.com  /mathree@yahoo.com </ul>
+
+          
+  
+              </li>
+              <p> Follow us on our Social Media Platforms for more information</p>
+
+              
+              </p>
+          </div>
+        </section>
+        </div>
+        </div>
+        </div>
+  )}
+
+ export default Contact
