3f8ce00e94e98ed5ff275ce1935508d78ea10b68
Snowflake <onyangogeorge954@gmail.com>
2023-01-10 07:26:03 +0300

    fix dependancy issue

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -1,72 +1,151 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import Info from "./Form/Info";
 import Payment from "./Form/Payment";
 import Summary from "./Form/Summary";
-import Stepper from "./Stepper";
-import {StepperContext} from "./context/StepperContext"
-import StepperControl from "./StepperControl";
+// import Stepper from "./Stepper";
+// import {StepperContext} from "./context/StepperContext"
+// import StepperControl from "./StepperControl";
 
-function Book() {
+// function Book() {
 
-  //define states for user details when making order
-  const [name, setName] = useState("");
-  const [contact, setContact] = useState("");
-  const [route, setRoute] = useState("");
-  const [pickup, setPickup] = useState("");
-  const [dropoff, setDropoff] = useState("");
-  const [returnTrip, setReturnTrip] = useState("");
+//   //define states for user details when making order
+//   const [name, setName] = useState("");
+//   const [contact, setContact] = useState("");
+//   const [route, setRoute] = useState("");
+//   const [pickup, setPickup] = useState("");
+//   const [dropoff, setDropoff] = useState("");
+//   const [returnTrip, setReturnTrip] = useState("");
 
-  const [currentStep, setCurrentStep] = useState(1);
-  const [userData, setUserData] = useState('');
-  const [finalData, setFinalData] = useState([]);
+//   const [currentStep, setCurrentStep] = useState(1);
+//   const [userData, setUserData] = useState('');
+//   const [finalData, setFinalData] = useState([]);
 
-  const steps = ["Personal Details", "Summary", "Payment"];
+//   const steps = ["Personal Details", "Summary", "Payment"];
 
-  const displaystep = (step) => {
+//   const displaystep = (step) => {
+//     switch (step) {
+//       case 1:
+//         return <Info />;
+//       case 2:
+//         return <Payment />;
+//       case 3:
+//         return <Summary />;
+//       default:
+//     }
+//   };
+
+//   const handleClick = (direction) => {
+//     let newStep = currentStep;
+
+//     direction === "next" ? newStep++ : newStep--;
+//     newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+//   };
+
+//   return (
+//     <div className="book-container">
+//       <div className="book-wrapper">
+//         <div className="book-route">
+//           <Stepper steps={steps} currentStep={currentStep} />
+
+//           {/*Display Components */}
+//           <div className="context">
+//             <StepperContext.Provider value={{
+//               userData,
+//               setUserData,
+//               finalData,
+//               setFinalData
+//             }}>
+//               {displaystep(currentStep)}
+//             </StepperContext.Provider>
+//           </div>
+//         </div>
+//         <StepperControl
+//           handleClick={handleClick}
+//           currentStep={currentStep}
+//           steps={steps}
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Book
+
+
+import React, { Component } from "react";
+import Success from "./Form/Success";
+
+
+export class Book extends Component {
+
+  state = {
+    step: 1,
+    fullName: '',
+    mobileNo: '',
+    pickLocation: '',
+    dropLocation: '',
+  }
+
+  // Move to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    })
+  }
+
+  // Go back to step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    })
+  }
+
+  // Handle fiels changes
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  }
+
+  render() {
+    const { step } = this.state;
+    const { firstName, lastName, homeAddress, pickTime, city } = this.state;
+    const values = { firstName, lastName, homeAddress, pickTime, city };
+
     switch (step) {
       case 1:
-        return <Info />;
+        return (
+          <Info
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
       case 2:
-        return <Payment />;
+        return (
+          <Summary
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
       case 3:
-        return <Summary />;
-      default:
+        return (
+          <Payment
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+          />
+        );
+      case 4:
+        return
+        (<Success />);
+
+        default : (console.log("Check if I am working"))
+
+
     }
-  };
+  }
 
-  const handleClick = (direction) => {
-    let newStep = currentStep;
-
-    direction === "next" ? newStep++ : newStep--;
-    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
-  };
-
-  return (
-    <div className="book-container">
-      <div className="book-wrapper">
-        <div className="book-route">
-          <Stepper steps={steps} currentStep={currentStep} />
-
-          {/*Display Components */}
-          <div className="context">
-            <StepperContext.Provider value={{
-              userData,
-              setUserData,
-              finalData,
-              setFinalData
-            }}>
-              {displaystep(currentStep)}
-            </StepperContext.Provider>
-          </div>
-        </div>
-        <StepperControl
-          handleClick={handleClick}
-          currentStep={currentStep}
-          steps={steps}
-        />
-      </div>
-    </div>
-  );
 }
 
-export default Book
+export default Book;
