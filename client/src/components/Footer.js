04822579644b4025e4ae27d6b5bcbd7c441b8447
charity <wa.nelimacharity@gmail.com>
2023-01-11 14:40:11 +0300

    addition of footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,35 +5,131 @@ import {faFacebook, faGithub,faYoutube,faTelegram, faTwitter,faTiktok,faInstagra
 function Footer() {
   return (
     
-    <div className='footer' >
+    
 
-        <div className='list'>
-            <p>+2541247654</p>
-            <p> mathree@gmail.com</p>
-            <p> Westlands,NRG Plaza,1st Floor</p>
+
+    
+  
+
+
+    
+    // <div className='footer' >
+
+    //     <div className='list'>
+    //         <p>+2541247654</p>
+    //         <p> mathree@gmail.com</p>
+    //         <p> Westlands,NRG Plaza,1st Floor</p>
      
                        
-        </div>
-        <hr/>
-    <div className='icons_container' >
+    //     </div>
+    //     <hr/>
+    // <div className='icons_container' >
 
 
-          <FontAwesomeIcon  icon  ={faYoutube} pull="left"></FontAwesomeIcon>
-          <FontAwesomeIcon icon ={faTwitter}></FontAwesomeIcon>
-          <FontAwesomeIcon icon ={faTelegram}></FontAwesomeIcon>
-          <FontAwesomeIcon icon ={faTiktok}></FontAwesomeIcon>
-          <FontAwesomeIcon icon ={faFacebook} ></FontAwesomeIcon>
-          <FontAwesomeIcon icon ={faInstagram} ></FontAwesomeIcon>
-          <FontAwesomeIcon icon ={faGithub}pull="right"></FontAwesomeIcon>
+    //       <FontAwesomeIcon  icon  ={faYoutube} pull="left"></FontAwesomeIcon>
+    //       <FontAwesomeIcon icon ={faTwitter}></FontAwesomeIcon>
+    //       <FontAwesomeIcon icon ={faTelegram}></FontAwesomeIcon>
+    //       <FontAwesomeIcon icon ={faTiktok}></FontAwesomeIcon>
+    //       <FontAwesomeIcon icon ={faFacebook} ></FontAwesomeIcon>
+    //       <FontAwesomeIcon icon ={faInstagram} ></FontAwesomeIcon>
+    //       <FontAwesomeIcon icon ={faGithub}pull="right"></FontAwesomeIcon>
         
 
 
 
 
-    </div>
-    </div>
+    // </div>
+    // </div>
     
+    // <div class="module"> 
+	// 			<h3>Company</h3> 
+	// 			<ul> 
+	// 				<li><a href="">Mission</a></li> 
+	// 				<li><a href="">History</a></li> 
+	// 				<li><a href="">Staff</a></li> 
+	// 				<li><a href="">Management</a></li> 
+	// 				<li><a href="">Contact</a></li> 
+	// 			</ul> 
+	// 		</div> 
+			
+
+  
+<div id="footer"> 
+	{/* <section id="cta"> 
+		<h2>Call-to-action</h2> 
+		<p>Call-to-action message body.</p> 
+		<form> 
+			<input type="text" placeholder="E-mail address" /> 
+			<input type="submit" value="Try now" /> 
+		</form> 
+	</section>  */}
+	<section id="resources"> 
+		<h2>Resources</h2> 
+		<div id="modules"> 
+			<h2>Resources</h2> 
+			<div class="module"> 
+				<h3>Product</h3> 
+               
+				<ul> 
+					<li><a href="">Pricing</a></li> 
+					<li><a href="">Features</a></li> 
+					<li><a href="">Documentation</a></li> 
+					<li><a href="">Developers</a></li> 
+                    <hr/>
+				</ul> 
+			</div> 
+			<div class="module"> 
+				<h3>MaThree</h3> 
+               
+				<ul> 
+					<li><a href="">Mission</a></li> 
+					<li><a href="">History</a></li> 
+					<li><a href="">Staff</a></li> 
+					<li><a href="">Management</a></li> 
+					{/* <li><a href="">Contact</a></li>  */}
+                    <hr/>
+				</ul> 
+			</div> 
+			<div class="module"> 
+				<h3>Contact</h3> 
+                
+				<ul> 
+					<li class="mail"><a href="">mathree@gmail.com</a></li> 
+					<li class="phone"><a href="">+2547 234 567 247</a></li> 
+					<li class="twitter"><a href="">@mathree</a></li> 
+					<li class="facebook"><a href="">mathree ltd</a></li> 
+                    <hr/>
+				</ul> 
+			</div> 
+			<div class="module"> 
+				<h3>Company</h3>
+                
+				
+				<ul>
+                    <li>1st Floor</li>
+                    <li>NRG Plaza</li> 
+					<li>Tom Mboya street 123</li> 
+					<li>Nairobi, Kenya</li> 
+                    <hr/> 
+				</ul> 
+              
+			</div> 
+		</div> 
+	</section> 
+	<div id="signature" aria-hidden="true"></div> 
+	<footer id="legal"> 
+		<p>&copy; 2023. All rights reserved. Please read the <a href="">Privacy Policy</a> and <a href="">Terms of Service</a>.</p> 
+	</footer> 
+</div> 
   )
 }
 
-export default Footer;
+export default Footer; 
+
+
+
+
+
+
+
+
