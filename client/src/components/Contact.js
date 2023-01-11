83aecb8a8bee719a2362b7ee7dba11ce122bf455
evans ochieng <evansochieng121@gmail.com>
2023-01-11 03:58:37 +0300

    remove summary from contact, show the logout button in the page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Summary from './Form/Summary';
 
 function Contact() {
   return (
     <div>
       <p style={{ paddingTop: "200px" }}>Our contacts will go here</p>
-      <Summary />
     </div>
   );
 }
