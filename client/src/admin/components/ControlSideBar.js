d95478b9f8dec7899b47ea6e968f49e402e4fb68
Riddick-Angiela <angielariddick@gmail.com>
2023-01-18 09:25:44 +0300

    admin dashboard slides

---
+++ client/src/admin/components/ControlSideBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ControlSideBar() {
+  return (
+    <div> 
+       {/* <!-- Control Sidebar --> */}
+  <aside class="control-sidebar control-sidebar-dark">
+    {/* <!-- Control sidebar content goes here --> */}
+    <div class="p-3">
+      {/* <!-- Content of the sidebar goes here --> */}
+    </div>
+  </aside>
+  {/* <!-- /.control-sidebar --> */}
+  </div>
+  )
+}
+
+export default ControlSideBar
